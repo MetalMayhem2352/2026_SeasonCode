9c9f03ff9f983fac1b4502c220be680d3b9eaa98
StrangeWind2352 <gabemanuel1213@gmail.com>
2026-03-09 01:04:13 -0500

    problem

---
+++ 2026_SeasonCode/src/main/include/Modules/TurretFeeder.h
@@ -0,0 +1,16 @@
+#include "Core/Timer.h"
+#include "Core/PIDController.h"
+#include "Constants.h"
+
+#include <core/LimelightHelpers.h>
+#include <ctre/phoenix6/TalonFX.hpp>
+#include <frc/Timer.h>
+
+class TurretFeeder
+{
+    private:
+        ctre::phoenix6::hardware::TalonFX* feederMotor;
+
+    public:
+
+};
