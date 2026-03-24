3a21243fad425f3c34e90195730a8ff122167a46
Asher Jordan <theasherbot@gmail.com>
2026-03-24 13:36:42 -0500

    Removed Hood Motor

--- 2026_SeasonCode/src/main/cpp/Module/ShooterModule.cpp
+++ 2026_SeasonCode/src/main/cpp/Module/ShooterModule.cpp
@@ -6,7 +6,6 @@ namespace Modules
     ShooterModule::ShooterModule()
     {
         shooterMotor = new ctre::phoenix6::hardware::TalonFX(Constants::Shooter::shooterID, Constants::CANIVOUR_NAME);
-        hoodMotor = new ctre::phoenix6::hardware::TalonFX(Constants::Shooter::hoodID, Constants::CANIVOUR_NAME);
     }
     ShooterModule::~ShooterModule()
     {
@@ -16,13 +15,11 @@ namespace Modules
     {
         shooterMotor->Set(1);
         currentState = State::Shoot;
-        // hoodMotor->Set(-0.05);
     }
     void ShooterModule::Stop()
     {
         currentState = Idle;
         shooterMotor->Set(0);
-        hoodMotor->Set(0);
     }
 
 
